593f98e765faa9f537db3878ff6dc0aa782f90ab
Bryan Wilton <bryanwilton27@gmail.com>
2021-09-15 17:37:01 -0300

    first commit

---
+++ sobre.js
@@ -0,0 +1,13 @@
+function entrar(id)
+{
+    let elemento = document.getElementById(id);
+    elemento.style.backgroundColor = 'white';
+    elemento.style.color = '#323232';
+}
+
+function sair(id)
+{
+    let elemento = document.getElementById(id);
+    elemento.style.backgroundColor = '#323232';
+    elemento.style.color = 'white';
+}
